4514a4028a160790508477c034c452f7b058c50c
Luan Campos <luan@nancy.corp>
2020-01-02 12:15:32 -0300

    Deletando e salvando no localStorage

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,11 +2,7 @@ var listElement = document.querySelector(".appList");
 var inputElement = document.querySelector("#app input");
 var buttonElement = document.querySelector("#app button");
 
-var todos = [
-    'Fazer Café',
-    'Estudar JavaScript',
-    'Acessar Blog da MicroCompanny'
-]
+var todos = JSON.parse(localStorage.getItem('todo_list')) || [];
 
 function renderTodo() {
     listElement.innerHTML = "";
@@ -17,14 +13,26 @@ function renderTodo() {
         checkBox.setAttribute('type', 'checkbox');
         checkBox.setAttribute('name', todo);
 
+        var linkEl = document.createElement('a');
+        linkEl.setAttribute('href', '#');
+        linkEl.setAttribute("class", "fas fa-trash");
+
+        var pos = todos.indexOf(todo);
+        linkEl.setAttribute('onclick', 'deleteTodo(' + pos + ')');
+
         var iElement = document.createElement('i');
 
         var spanElement = document.createElement('span');
         spanElement.appendChild(document.createTextNode(todo));
 
+
+        // var textLink = document.createTextNode("Excluir");
+        // linkEl.appendChild(textLink);
+
         todoElement.appendChild(checkBox);
         todoElement.appendChild(iElement);
         todoElement.appendChild(spanElement);
+        todoElement.appendChild(linkEl);
 
         listElement.appendChild(todoElement);
 
@@ -43,7 +51,20 @@ function addTodo() {
         inputElement.value = "";
 
         renderTodo();
+        saveToStorage();
     }
 }
 
 buttonElement.onclick = addTodo;
+
+function deleteTodo(pos) {
+    todos.splice(pos, 1);
+
+    renderTodo();
+    saveToStorage();
+}
+
+
+function saveToStorage() {
+    localStorage.setItem('todo_list', JSON.stringify(todos));
+}
